31869d2cbbc37595b6ec1c0ef659655adf78de85
Kong <kongPro@Kongs-MacBook-Pro-7.local>
2017-09-21 10:05:13 -0500

    second commit

--- DirectedFlowCorrelator/plugins/DirectedFlowCorrelator.cc
+++ DirectedFlowCorrelator/plugins/DirectedFlowCorrelator.cc
@@ -17,59 +17,50 @@
 //
 
 
-// system include files
-#include <memory>
-
-// user include files
-#include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/one/EDAnalyzer.h"
-
-#include "FWCore/Framework/interface/Event.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "FWCore/ParameterSet/interface/ParameterSet.h"
-//
-// class declaration
-//
-
-// If the analyzer does not use TFileService, please remove
-// the template argument to the base class so the class inherits
-// from  edm::one::EDAnalyzer<> and also remove the line from
-// constructor "usesResource("TFileService");"
-// This will improve performance in multithreaded jobs.
-
-class DirectedFlowCorrelator : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
-   public:
-      explicit DirectedFlowCorrelator(const edm::ParameterSet&);
-      ~DirectedFlowCorrelator();
-
-      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+#include "DirectedFlowCorrelator/DirectedFlowCorrelator/interface/DirectedFlowCorrelatorBase.h"
 
 
-   private:
-      virtual void beginJob() override;
-      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-      virtual void endJob() override;
-
-      // ----------member data ---------------------------
-};
-
-//
-// constants, enums and typedefs
-//
-
-//
-// static data member definitions
-//
-
-//
-// constructors and destructor
-//
 DirectedFlowCorrelator::DirectedFlowCorrelator(const edm::ParameterSet& iConfig)
 
 {
-   //now do what ever initialization is needed
-   usesResource("TFileService");
+  trackName_  =  iConfig.getParameter<edm::InputTag>("trackName");
+  vertexName_ =  iConfig.getParameter<edm::InputTag>("vertexName");
+  towerName_ =  iConfig.getParameter<edm::InputTag>("towerName");
+
+  trackSrc_ = consumes<reco::TrackCollection>(trackName_);
+  vertexSrc_ = consumes<reco::VertexCollection>(vertexName_);
+  towerSrc_ = consumes<CaloTowerCollection>(towerName_);
+
+  Nmin_ = iConfig.getUntrackedParameter<int>("Nmin");
+  Nmax_ = iConfig.getUntrackedParameter<int>("Nmax");
+
+  useCentrality_ = iConfig.getUntrackedParameter<bool>("useCentrality");
+  doEffCorrection_ = iConfig.getUntrackedParameter<bool>("doEffCorrection");
+  useEtaGap_ = iConfig.getUntrackedParameter<bool>("useEtaGap");
+  doBothSide_ = iConfig.getUntrackedParameter<bool>("doBothSide");
+
+  eff_ = iConfig.getUntrackedParameter<int>("eff");
+
+  etaTracker_ = iConfig.getUntrackedParameter<double>("etaTracker");
+  gapValue_ = iConfig.getUntrackedParameter<double>("gapValue");
+  
+  etaLowHF_ = iConfig.getUntrackedParameter<double>("etaLowHF");
+  etaHighHF_ = iConfig.getUntrackedParameter<double>("etaHighHF");
+
+  vzLow_ = iConfig.getUntrackedParameter<double>("vzLow");
+  vzHigh_ = iConfig.getUntrackedParameter<double>("vzHigh");
+  
+  ptLow_ = iConfig.getUntrackedParameter<double>("ptLow");
+  ptHigh_ = iConfig.getUntrackedParameter<double>("ptHigh");
+
+  offlineptErr_ = iConfig.getUntrackedParameter<double>("offlineptErr", 0.0);
+  offlineDCA_ = iConfig.getUntrackedParameter<double>("offlineDCA", 0.0);
+  offlineChi2_ = iConfig.getUntrackedParameter<double>("offlineChi2", 0.0);
+  offlinenhits_ = iConfig.getUntrackedParameter<double>("offlinenhits", 0.0);
+
+  etaBins_ = iConfig.getUntrackedParameter<std::vector<double>>("etaBins");
+  ptBins_ = iConfig.getUntrackedParameter<std::vector<double>>("ptBins");
+  centBins_ = iConfig.getUntrackedParameter<std::vector<double>>("centBins");
 
 }
 
@@ -91,19 +82,90 @@ DirectedFlowCorrelator::~DirectedFlowCorrelator()
 void
 DirectedFlowCorrelator::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-   using namespace edm;
+  using namespace edm;
+  using namespace std;
+
+  edm::Handle<reco::VertexCollection> vertices;
+  iEvent.getByToken(vertexSrc_,vertices);
+  double bestvz=-999.9, bestvx=-999.9, bestvy=-999.9;
+  double bestvzError=-999.9, bestvxError=-999.9, bestvyError=-999.9;
+  const reco::Vertex & vtx = (*vertices)[0];
+  bestvz = vtx.z(); 
+  bestvx = vtx.x(); 
+  bestvy = vtx.y();
+  bestvzError = vtx.zError(); 
+  bestvxError = vtx.xError(); 
+  bestvyError = vtx.yError();
+
+  //first selection; vertices
+  if( fabs(bestvz) < vzLow_ || fabs(bestvz) > vzHigh_ ) return;
+
+  vtxZ->Fill( bestvz );
+
+  Handle<CaloTowerCollection> towers;
+  iEvent.getByToken(towerSrc_, towers);
+
+  Handle<reco::TrackCollection> tracks;
+  iEvent.getByToken(trackSrc_, tracks);
+
+  int nTracks = 0;
+  for(unsigned it = 0; it < tracks->size(); it++){
+
+     const reco::Track & trk = (*tracks)[it];
+  
+     math::XYZPoint bestvtx(bestvx,bestvy,bestvz);
+
+        double dzvtx = trk.dz(bestvtx);
+        double dxyvtx = trk.dxy(bestvtx);
+        double dzerror = sqrt(trk.dzError()*trk.dzError()+bestvzError*bestvzError);
+        double dxyerror = sqrt(trk.d0Error()*trk.d0Error()+bestvxError*bestvyError);
+
+        if(!trk.quality(reco::TrackBase::highPurity)) continue;
+        if(fabs(trk.ptError())/trk.pt() > 0.1 ) continue;
+        if(fabs(dzvtx/dzerror) > 3.0) continue;
+        if(fabs(dxyvtx/dxyerror) > 3.0) continue;
+        if(trk.pt() < 0.4 || fabs(trk.eta()) > 2.4) continue;
+        nTracks++;//count multiplicity
+
+  }
+
+  if( !useCentrality_ ) if( nTracks < Nmin_ || nTracks >= Nmax_ ) return;
+
+  double etHFtowerSumPlus = 0.0;
+  double etHFtowerSumMinus = 0.0;
+  double etHFtowerSum = 0.0;
+  
+  if( useCentrality_ ){
+
+    for( unsigned i = 0; i<towers->size(); ++ i){
+       const CaloTower & tower = (*towers)[ i ];
+       double eta = tower.eta();
+       bool isHF = tower.ietaAbs() > 29;
+          if(isHF && eta > 0){
+            etHFtowerSumPlus += tower.pt();
+          }
+          if(isHF && eta < 0){
+            etHFtowerSumMinus += tower.pt();
+          }
+    }
+    etHFtowerSum=etHFtowerSumPlus + etHFtowerSumMinus;
+
+    int bin = -1;
+    for(int j=0; j<200; j++){
+      if( etHFtowerSum >= centBins_[j] ){
+         bin = j; break;
+      }
+    }
+
+    int hiBin = bin;
+    if( hiBin < Nmin_ || hiBin >= Nmax_ ) return;
+    cbinHist->Fill( hiBin );
+
+  }
+
+  Ntrk->Fill( nTracks );
 
 
-
-#ifdef THIS_IS_AN_EVENT_EXAMPLE
-   Handle<ExampleData> pIn;
-   iEvent.getByLabel("example",pIn);
-#endif
-   
-#ifdef THIS_IS_AN_EVENTSETUP_EXAMPLE
-   ESHandle<SetupData> pSetup;
-   iSetup.get<SetupRecord>().get(pSetup);
-#endif
 }
 
 
@@ -111,6 +173,30 @@ DirectedFlowCorrelator::analyze(const edm::Event& iEvent, const edm::EventSetup&
 void 
 DirectedFlowCorrelator::beginJob()
 {
+  edm::Service<TFileService> fs;
+    
+  TH1D::SetDefaultSumw2();
+
+  double ptBinsArray[100];
+  const int Nptbins = ptBins_.size() - 1;
+  for(unsigned i = 0; i < ptBins_.size(); i++){
+    ptBinsArray[i] = ptBins_[i];
+  }
+
+  edm::FileInPath fip1("DirectedFlowCorrelator/DirectedFlowCorrelator/data/Hydjet_eff_mult_v1.root");
+  TFile f1(fip1.fullPath().c_str(),"READ");
+  for(int i = 0; i < 5; i++){
+     effTable[i] = (TH2D*)f1.Get(Form("rTotalEff3D_%d",i));
+  }
+
+  Ntrk = fs->make<TH1D>("Ntrk",";Ntrk",5000,0,5000);
+  vtxZ = fs->make<TH1D>("vtxZ",";vz", 400,-20,20);
+  cbinHist = fs->make<TH1D>("cbinHist",";cbin",200,0,200);
+  trkPhi = fs->make<TH1D>("trkPhi", ";#phi", 700, -3.5, 3.5);
+  hfPhi = fs->make<TH1D>("hfPhi", ";#phi", 700, -3.5, 3.5);
+  trkPt = fs->make<TH1D>("trkPt", ";p_{T}(GeV)", Nptbins,ptBinsArray);
+  trk_eta = fs->make<TH1D>("trk_eta", ";#eta", 50,-2.5,2.5);
+
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -122,11 +208,6 @@ DirectedFlowCorrelator::endJob()
 // ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
 void
 DirectedFlowCorrelator::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
-  //The following says we do not know what parameters are allowed so do no validation
-  // Please change this to state exactly what you do use, even if it is no parameters
-  edm::ParameterSetDescription desc;
-  desc.setUnknown();
-  descriptions.addDefault(desc);
 }
 
 //define this as a plug-in
